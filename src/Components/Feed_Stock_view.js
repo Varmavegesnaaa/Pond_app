316b0e33547ff5338c2533449a997ddf07fc5633
Varmavegesnaaa <98891909+Varmavegesnaaa@users.noreply.github.com>
2024-09-11 19:13:29 +0530

    Update Feed_Stock_view.js

--- src/Components/Feed_Stock_view.js
+++ src/Components/Feed_Stock_view.js
@@ -15,7 +15,7 @@ const FeedListComponent = () => {
   useEffect(() => {
     const fetchFeeds = async () => {
       try {
-        const response = await fetch('https://pond-app-backend.vercel.app/feeds');
+        const response = await fetch('https://pond-app-backend-z2yl.vercel.app/feeds');
         const data = await response.json();
 
         // Log data to verify its structure
